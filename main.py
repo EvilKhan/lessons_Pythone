eb161aec1afc377fb444ea65a5bcf107ab2c9439
UsalTatar <barbariandiii@gmail.com>
2023-02-21 13:19:04 +0700

    third q

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#Найдите сумму цифр трехзначного числа.
-#Пример:
-#123 -> 6 (1 + 2 + 3)
-#100 -> 1 (1 + 0 + 0)
-#digit = input('Введите трехзначное число: ')
-#result = int(digit[0]) + int(digit[1]) + int(digit[2])
-#print(digit,'->',result)
+# Найдите сумму цифр трехзначного числа.
+# Пример:
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+# digit = input('Введите трехзначное число: ')
+# result = int(digit[0]) + int(digit[1]) + int(digit[2])
+# print(digit,'->',result)
 
 # Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов.
 # Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов,
@@ -13,7 +13,22 @@
 # 6 -> 1  4  1
 # 24 -> 4  16  4
 # 60 -> 10  40  10
-total_birds = int(input('Введите количество журавликов: '))
-kate_birds = int(total_birds / 3 * 2)
-boys_birds = int((total_birds - kate_birds)/2)
-print(F"{total_birds} -> {boys_birds}  {kate_birds}  {boys_birds}")
+# total_birds = int(input('Введите количество журавликов: '))
+# kate_birds = int(total_birds / 3 * 2)
+# boys_birds = int((total_birds - kate_birds)/2)
+# print(F"{total_birds} -> {boys_birds}  {kate_birds}  {boys_birds}")
+
+# Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером.
+# Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме
+# последних трех. Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6.
+# Вам требуется написать программу, которая проверяет счастливость билета.
+# Пример:
+# 385916 -> yes
+# 123456 -> no
+ticket_number = input('Введите номер билета: ')
+first_half = int(ticket_number[0]) + int(ticket_number[1]) + int(ticket_number[2])
+second_half = int(ticket_number[3]) + int(ticket_number[4]) + int(ticket_number[5])
+if first_half == second_half:
+    print('yes')
+else:
+    print('no')
